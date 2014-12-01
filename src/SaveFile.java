1697864566d97fbf0c016e7494cca665e1e3526e
mattcarabine <matt.carabine@hotmail.co.uk>
2014-12-01 00:20:01 +0000

    Begun work on creating a SaveFile module, copied loadfile code for now.

---
+++ src/SaveFile.java
@@ -0,0 +1,101 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class SaveFile extends JDialog {
+    public boolean okClicked = false;
+    private ArrayList<Station> nodes;
+    public SaveFile(){
+        this.setModal(true);
+        this.getContentPane().add (new SaveFile());
+        this.pack();
+        this.setVisible (true);
+    }
+
+    public class SavePanel extends JPanel {
+        private JTextField tbFileName;
+        private JLabel jcomp2;
+        private JButton btnBrowse;
+        private JButton btnOK;
+        private JButton btnCancel;
+        public SavePanel() {
+            tbFileName = new JTextField (5);
+            jcomp2 = new JLabel ("Enter location of file you wish to save to:");
+            btnBrowse = new JButton ("Browse...");
+            btnOK = new JButton ("OK");
+            btnCancel = new JButton ("Cancel");
+            setPreferredSize (new Dimension (635, 110));
+            setLayout (null);
+            add (tbFileName);
+            add (jcomp2);
+            add (btnBrowse);
+            add (btnOK);
+            add (btnCancel);
+            btnBrowse.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    final JFileChooser fc = new JFileChooser();
+                    int returnVal = fc.showOpenDialog(getParent());
+                    if (returnVal == JFileChooser.APPROVE_OPTION) {
+                        tbFileName.setText(fc.getSelectedFile().getAbsolutePath().toString());
+                    }
+                }
+            });
+            btnOK.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (new File(tbFileName.getText()).isFile()) {
+                        load(tbFileName.getText());
+                        okClicked = true;
+                        close();
+                    } else {
+
+                    }
+                }
+            });
+            btnCancel.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    close();
+                }
+            });
+            tbFileName.setBounds (25, 35, 470, 25);
+            jcomp2.setBounds (25, 5, 240, 30);
+            btnBrowse.setBounds (505, 35, 100, 25);
+            btnOK.setBounds (415, 75, 100, 25);
+            btnCancel.setBounds (525, 75, 100, 25);
+        }
+        private void close(){
+            SaveFile.this.setVisible(false);
+        }
+    }
+    private void load(String fileName){
+        ArrayList<Station> loadedNodes = new ArrayList<Station>();
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader(
+                    fileName));
+            JSONArray nodeList = (JSONArray) obj;
+
+            for (int i =0; i<nodeList.size();i++){
+                JSONObject nodeJSON = (JSONObject) nodeList.get(i);
+                Station node = new Station(i,nodeJSON.get("name").toString(),new Point(nodeJSON.get("location").toString()));
+                loadedNodes.add(node);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        this.nodes = loadedNodes;
+    }
+    public ArrayList<Station> getNodes(){
+        return this.nodes;
+    }
+}
